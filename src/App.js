e2b4352cd2c16d6c36a7daa40a478af9af783796
Miles Higbie <miles.higbie@gmail.com>
2020-05-29 14:10:47 -0400

    set x values to state, set env key

--- src/App.js
+++ src/App.js
@@ -5,27 +5,20 @@ import Stock from './components/Stock.js';
 
 
 // const result = dotenv.config()
-//D0AZSV1R7TJHZJN2
+
 
 
 function App() {
 
+  console.log()
   const [backside, setBackside] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3000/api/tutorials")
     .then(response => response.json())
     .then(response => {
-      console.log(response)
       setBackside(response)
     })
-
-    fetch(" https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=D0AZSV1R7TJHZJN2")
-    .then(response => response.json())
-    .then(response => {
-      console.log(response)
-    })
-
   },[]);
 
   return (
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 function Stock() {
+
+    const [stockXValues, setStockXValues] = useState([])
+    const [stockYValues, setStockYValues] = useState([])
+
+
+    useEffect(() => {
+        fetchStock()
+    }, []);
+
+    // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=D0AZSV1R7TJHZJN2
+
+    const fetchStock = () => {
+        const symbol = "IBM"
+        const apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.REACT_API_ALPHA_API_KEY}`
+        
+        fetch(apiCall)
+        .then(response => response.json())
+        .then(response => {
+            let dates = []
+            Object.keys(response["Time Series (Daily)"]).map(date => {
+                 dates = [...dates,date]
+             })
+            setStockXValues(dates)
+        })
+    }
+
   return (
     <div>
-        <h1> Hello Ocean! </h1>
+        {stockXValues.map(date => <p> {date} </p>)}
     </div>
   );
 }
