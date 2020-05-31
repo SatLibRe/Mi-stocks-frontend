69183a959968de43657798b2daebc19de41489bf
Miles Higbie <miles.higbie@gmail.com>
2020-05-31 14:39:48 -0400

    added error handleing for wrong stock tickers

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -5,6 +5,7 @@ function Stock() {
 
     const [stockXValues, setStockXValues] = useState([])
     const [stockYValues, setStockYValues] = useState([])
+    const [symbol, setSymbol] = useState("SPY")
 
 
     useEffect(() => {
@@ -12,27 +13,38 @@ function Stock() {
     }, []);
 
     const fetchStock = () => {
-        const symbol = "IBM"
         const apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.REACT_API_ALPHA_API_KEY}`
         
         fetch(apiCall)
         .then(response => response.json())
         .then(response => {
-            let dates = []
-            let opens = []
+            if(response["Error Message"]){
+                alert('That is not a Stock')
+            } else {
+                let dates = []
+                let opens = []
 
-            for(const key in response["Time Series (Daily)"]){
-                dates.push(key)
-                opens.push(response["Time Series (Daily)"][key]["1. open"])
+                for(const key in response["Time Series (Daily)"]){
+                    dates.push(key)
+                    opens.push(response["Time Series (Daily)"][key]["1. open"])
+                }
+                setStockXValues(dates)
+                setStockYValues(opens)
             }
-            setStockXValues(dates)
-            setStockYValues(opens)
         })
     }
 
+    const handleSymbolChange = (event) => {
+        setSymbol(event.target.value);
+      }
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        fetchStock()
+      }
+
   return (
     <div>
-        {/* {stockXValues.map(date => <p> {date} </p>)} */}
         <Plot
         data={[
           {
@@ -43,8 +55,15 @@ function Stock() {
             marker: {color: 'red'},
           },
         ]}
-        layout={ {width: 720, height: 340, title: ''} }
+        layout={ {width: 720, height: 340, title: `${symbol}`} }
       />
+         <form onSubmit={handleSubmit}>
+        <label>
+          Ticker:
+          <input type="text" value={symbol} onChange={handleSymbolChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
