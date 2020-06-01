5390dc418a9a9718f4efcbc2e82c5377ba2d2f50
Miles Higbie <miles.higbie@gmail.com>
2020-06-01 15:38:33 -0400

    added key for fin api

--- src/containers/StockList.js
+++ src/containers/StockList.js
@@ -3,6 +3,16 @@ import React, { useState, useEffect } from 'react';
 
 function StockList() {
 
+    console.log(process.env)
+
+    useEffect(() => {
+        fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${process.env.REACT_APP_FIN_API_KEY}`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+        })
+    },[])
+
   return (
     <div id="stock-list-div">
         
